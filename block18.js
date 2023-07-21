eefc56b7fc09ba985601b9cc036273859aa58f0e
kaykyolee <kleegoma@gmail.com>
2023-07-21 03:39:51 -0400

    uploading block18 writing test specifications assignment

---
+++ block18.js
@@ -0,0 +1,28 @@
+// Unit Tests:
+// 1. A function called "multiplication" that returns the product of the two input numbers.
+
+// - Expect the multiplication of two input numbers to be entered in number value, which will be in integers 
+// - Expect the multiplication of two input numbers to return error if any of the numbers are entered with string ('' or "")
+// - Expect the multiplication of two input numbers to return error if more or less than two numbers are inputted 
+// - Expect the multiplication of two input numbers to return mathematically accurate multiplication calculation of two number values 
+// - Expect the multiplication of two input numbers to return a singular number value as a result
+
+// 2. A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+
+// - Expect a function called "concatOdds" to be have all values entered in number value, which will be in integers
+// - Expect a function called "concatOdds" to return error if any of the numbers are entered with string ('' or "")
+// - Expect a function called "concatOdds" to return an array that goes from ascending order, not descending order
+// - Expect a function called "concatOdds" to return no even numbers in the array
+// - Expect a function called "concatOdds" to return no value if there are no odd numbers in the two arrays inputted 
+// - Expect a function called "concatOdds" to not return any 0 values in the two arrays inputted  
+
+// Functional Tests:
+// 1. A shopping cart checkout feature that allows a user to check out as a guest (without an account), or as a logged-in user. They should be allowed to do either, but should be asked if they want to create an account or log in if they check out as a guest.
+
+// - When a user clicks the checkout page, they should be given options to check out as a guest, log in as an existing user, or sign up to be a new user
+// - When a user clicks the check out as a guest option, they should see the form to fill out their basic person information such as name, email, address, phone Number, payment information
+// - When a user clicks the log in as an existing user option, they should see the authentication page to fill out their email/username and password
+// - When an existing user entered the email/username and password, they should automatically get the security code to the preferred communication method in file and should be able to enter the authentication code 
+// - when a user clicks the sign up to be a new user option, they should be able to enter their personal details such as name, email, address, phone Number, preferred method of communication 
+// - When a new user that is trying to sign up has entered all their personal details, they should get the checkbox to confirm that they have agreed to the terms 
+// - When a new user that is trying to sign up has agreed to the terms, they should get a confirmation email to their email address to confirm the creation of account 
